27e7af2cbdb44a234595d190ad712ea5af809d44
ccumaco <carloscumaco5@gmail.com>
2022-03-03 23:57:48 -0500

    start commerce ok

--- src/routes/comercios.js
+++ src/routes/comercios.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+const mysqlConnection = require('../database.js');
 
 
 router.get('/comercios', (req, res) => {
   mysqlConnection.query('SELECT * FROM comercios', (err, rows, fields) => {
-    if(!err) {
+    if (!err) {
       res.json(rows);
     } else {
       console.log(err);
     }
-  });  
+  });
 });
 router.get('/usuarios', (req, res) => {
   mysqlConnection.query('SELECT * FROM usuarios', (err, rows, fields) => {
-    if(!err) {
+    if (!err) {
       res.json(rows);
     } else {
       console.log(err);
     }
-  });  
+  });
 });
 
 // GET An comercios
-router.get('/:idcomercios', (req, res) => {
-  const { idcomercios } = req.params; 
-  mysqlConnection.query('SELECT * FROM comercios WHERE idcomercios = ?', [idcomercios], (err, rows, fields) => {
+router.get('/:idcomercio', (req, res) => {
+  const { idcomercio } = req.params;
+  mysqlConnection.query('SELECT * FROM comercios WHERE idcomercio = ?', [idcomercio], (err, rows, fields) => {
     if (!err) {
       res.json(rows[0]);
     } else {
@@ -36,11 +36,11 @@ router.get('/:idcomercios', (req, res) => {
 });
 
 // DELETE An comercios
-router.delete('/:idcomercios', (req, res) => {
-  const { idcomercios } = req.params;
-  mysqlConnection.query('DELETE FROM comercios WHERE idcomercios = ?', [idcomercios], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'comercio eliminado con el id:' + idcomercios});
+router.delete('/:idcomercio', (req, res) => {
+  const { idcomercio } = req.params;
+  mysqlConnection.query('DELETE FROM comercios WHERE idcomercio = ?', [idcomercio], (err, rows, fields) => {
+    if (!err) {
+      res.json({ status: 'comercio eliminado con el id:' + idcomercio });
     } else {
       console.log(err);
     }
@@ -49,13 +49,13 @@ router.delete('/:idcomercios', (req, res) => {
 
 // INSERT An comercios
 router.post('/comercio', (req, res) => {
-  const {idcomercios, nombre, usuario_id, apellido, edad, salario, calificacion} = req.body;
+  const { idcomercio, nombre, usuario_id, apellido, edad, salario, calificacion } = req.body;
   const query = `
-    CALL comerciosAddOrEdit(?, ?, ?, ?, ?, ?,?);
+  INSERT INTO comercios.comercios (idcomercio, nombre, usuario_id,apellido,edad,salario,calificacion)  values( ?, ?, ?, ?, ?, ?, ? );
   `;
-  mysqlConnection.query(query, [idcomercios, nombre,usuario_id, apellido, edad, salario, calificacion], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'comercio creado con el id:'+ req.body.idcomercios});
+  mysqlConnection.query(query, [idcomercio, nombre, usuario_id, apellido, edad, salario, calificacion], (err, rows, fields) => {
+    if (!err) {
+      res.json({ status: 'comercio creado con el id:' + req.body.idcomercio });
     } else {
       console.log(err);
     }
@@ -65,31 +65,33 @@ router.post('/comercio', (req, res) => {
 
 // INSERT An usuario
 router.post('/usuario', (req, res) => {
-    const {idusuarios, nombreUsuario,semestreUsuario} = req.body;
-    const query = `
-        INSERT INTO instructores.usuarios (idusuarios, nombreUsuario, semestreUsuario)  values( ?, ?, ? );
-    `;
-    mysqlConnection.query(query, [idusuarios, nombreUsuario,semestreUsuario], (err, rows, fields) => {
-      if(!err) {
-        res.json({status: 'usuario creada con el id:'+ req.body.idusuarios});
-      } else {
-        console.log(err);
-      }
-    });
-  
-  });
-
-router.put('/:idcomercios', (req, res) => {
-    const {nombre, usuario_id,apellido, edad, salario, calificacion} = req.body;
-  const { idcomercios } = req.params;
+  const { idusuario, nombreUsuario, descripcionUsuario } = req.body;
   const query = `
-    CALL comerciosAddOrEdit(?,?, ?, ?,?, ?, ?);
-  `;
-  mysqlConnection.query(query, [idcomercios, nombre, usuario_id,apellido, edad, salario, calificacion], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'comercio actualizado con el id' + idcomercios});
+        INSERT INTO comercios.usuarios (idusuario, nombreUsuario, descripcionUsuario)  values( ?, ?, ? );
+    `;
+  mysqlConnection.query(query, [idusuario, nombreUsuario, descripcionUsuario], (err, rows, fields) => {
+    if (!err) {
+      res.json({ status: 'usuario creada con el id:' + req.body.idusuario });
     } else {
       console.log(err);
+    }
+  });
+
+});
+
+
+
+
+
+router.put('/:idcomercio', (req, res) => {
+  const { nombre, usuario_id, apellido, edad, salario, calificacion } = req.body;
+  const { idcomercio } = req.params;
+  const query = `UPDATE comercios SET nombre = ?, usuario_id = ?, apellido = ?, edad = ?, salario = ?, calificacion = ? WHERE idcomercio = ?`;
+  mysqlConnection.query(query, [nombre, usuario_id, apellido, edad, salario, calificacion, idcomercio], (err, rows, fields) => {
+    if (!err) {
+      res.json({ status: 'comercio actualizado con el id' + idcomercio });
+    } else {
+      res.json({ status: 'error' });
     }
   });
 });
