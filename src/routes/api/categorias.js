62945705106746ff87a9de5b0b8de68db098b7cf
ccumaco <carloscumaco5@gmail.com>
2022-03-05 01:08:22 -0500

    all crud categories

--- src/routes/api/categorias.js
+++ src/routes/api/categorias.js
@@ -2,92 +2,43 @@ const express = require('express');
 const router = express.Router();
 const { Categorias } = require('./../../database');
 
+
+// GET all comercios
 router.get('/', async (req, res) => {
   const categorias = await Categorias.findAll();
   res.json(categorias);
 })
 
+// crear comercio
 router.post('/', (req, res) => {
   const categorias = Categorias.create(req.body);
   res.json(categorias);
 })
 
 
+// actualizar comercio
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const categorias = await Categorias.findByPk(id);
+  if (categorias) {
+    const { nombre, descripcion, active, promotion, calificacion, image } = req.body;
+    const updated = await categorias.update({ nombre, descripcion, active, promotion, calificacion, image });
+    res.json(updated);
+  } else {
+    res.json({ message: 'No se encontro la categoria' });
+  }
+})
 
 
-
-
-
-
-
-
-
-// router.get('/get-categorias', (req, res) => {
-//   mysqlConnection.query('SELECT * FROM categorias', (err, rows, fields) => {
-//     if (!err) {
-//       res.json(rows);
-//     } else {
-//       console.log(err);
-//     }
-//   });
-// });
-
-// // GET An categorias
-// router.get('/categoria/:idcategorias', (req, res) => {
-//   const { idcategorias } = req.params;
-//   mysqlConnection.query('SELECT * FROM categorias WHERE idcategorias = ?', [idcategorias], (err, rows, fields) => {
-//     if (!err) {
-//       res.json(rows[0]);
-//     } else {
-//       console.log(err);
-//     }
-//   });
-// });
-
-// // DELETE An categorias
-// router.delete('/delete/:idcategorias', (req, res) => {
-//   const { idcategorias } = req.params;
-//   mysqlConnection.query('DELETE FROM categorias WHERE idcategorias = ?', [idcategorias], (err, rows, fields) => {
-//     if (!err) {
-//       res.json({ status: 'comercio eliminado con el id:' + idcategorias });
-//     } else {
-//       console.log(err);
-//     }
-//   });
-// });
-
-// // INSERT An categorias
-// router.post('/create-categoria', (req, res) => {
-//   const { idcategorias, nombre,  descripcion, active, promocion, imagen } = req.body;
-//   const query = `
-//   INSERT INTO categorias (idcategorias, nombre, descripcion, active, promocion, imagen)  values( ?, ?, ?, ?, ?, ?,? );
-//   `;
-//   mysqlConnection.query(query, [idcategorias, nombre, descripcion, active, promocion, imagen], (err, rows, fields) => {
-//     if (!err) {
-//       res.json({ status: 'comercio creado con el id:' + req.body.idcategorias });
-//     } else {
-//       console.log(err);
-//     }
-//   });
-
-// });
-
-
-
-
-
-
-// router.put('/categoria/:idcategorias', (req, res) => {
-//   const { nombre, usuario_id, descripcion, active, promocion, calificacion, imagen } = req.body;
-//   const { idcategorias } = req.params;
-//   const query = `UPDATE categorias SET nombre = ?, usuario_id = ?, descripcion = ?, active = ?, promocion = ?, calificacion = ?, imagen =?, WHERE idcategorias = ?`;
-//   mysqlConnection.query(query, [nombre, usuario_id, descripcion, active, promocion, calificacion, imagen, idcategorias], (err, rows, fields) => {
-//     if (!err) {
-//       res.json({ status: 'comercio actualizado con el id' + idcategorias });
-//     } else {
-//       res.json({ status: 'error' });
-//     }
-//   });
-// });
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const categorias = await Categorias.findByPk(id);
+  if (categorias) {
+    const deleted = await categorias.destroy();
+    res.json(deleted);
+  } else {
+    res.json({ message: 'No se encontro la categoria' });
+  }
+})
 
 module.exports = router;
